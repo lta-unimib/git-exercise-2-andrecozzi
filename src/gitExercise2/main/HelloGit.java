7c950b246d319a08751ee32d0e0dc0dd1c2772b5
andrecozzi <164370793+andrecozzi@users.noreply.github.com>
2024-10-22 15:12:41 +0200

    Sono il secondo developer e ho modificato una riga di codice

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -3,6 +3,6 @@ package gitExercise2.main;
 public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Hello git!!!");
-        System.out.println("This is another print")
+        System.out.println("MODIFICATA")
 	}
 }
